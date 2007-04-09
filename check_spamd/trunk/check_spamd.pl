18d171ee347f04f3204051acc25234bf9036c79f
erinn.looneytriggs <erinn.looneytriggs@53429630-192d-0410-916e-713da7112205>
2007-04-09 02:18:34 +0000

    Moving toward 1.5, moved to getopt::long much more flexible, trying to hash out the eval issues.

--- check_spamd/trunk/check_spamd.pl
+++ check_spamd/trunk/check_spamd.pl
@@ -4,27 +4,32 @@ Program to check to make sure spamd is running and report back to nrpe for
 nagios. The variable that most folks will want to change is "$spamc" which
 is the location of the spamc program.
 Created: 11/29/2006
-Version: 1.4              
+Version: 1.5              
 Revised: 4/02/2007
 Revised by: Erinn Looney-Triggs
 Author: Erinn Looney-Triggs
 =cut
 
-use Getopt::Std;             #Grab short command line switches
+use Getopt::Long;            #Grab command line switches
 use POSIX qw( WIFEXITED );   #Fix system call's strange return values
 use strict;                  #Do it right
 use Switch;                  #Standard perl 5.8 module to use switch statement
 
-my %flags;                   #Command line switches
-my $spamc = "/usr/local/perl/bin/spamc";    #Location of spamc
-$main::VERSION                      = "1.4";    #Version number
-$Getopt::Std::STANDARD_HELP_VERSION = "1";        #Die on help
+my %flags;                  #Command line switches hash
+$flags{timeout} = 10;       #Default timeout of 10 seconds
+my $return_code;            #Return code holder
+my $spamc = "/usr/local/perl/bin/spamc";        #Location of spamc
 my $spamc_command = "echo foo | $spamc -x 2>&1 > /dev/null";    #The command
-my $return_code;
+my $version = "1.5";        #Version number
 
-#-t flag to set timeout value, unless defined defaults to 10 seconds
-getopts( 't:', \%flags );
-$flags{t} = "10" unless ( $flags{t} );
+
+
+&Getopt::Long::Configure("bundling", "gnu_compat",);
+
+GetOptions (\%flags, 'timeout=i',
+            'version' => sub { VersionMessage() },
+            );
+
 
 #Make sure spamc exists and if not give back a nagios warning
 if ( !-e $spamc ) {
@@ -37,19 +42,20 @@ eval {
 
     #Set the alarm and set the timeout
     local $SIG{ALRM} = sub { die "alarm\n" };
-    alarm $flags{t};
+    alarm $flags{timeout};
 
     #Run the command
-    WIFEXITED( system("$spamc_command") )
+    WIFEXITED(system $spamc_command)
         or die "Could not run $spamc_command\n";
-
+    $return_code = $?;
+        
     alarm 0;
 };
 
 #Test return value and exit if eval caught the alarm
 if ($@) {
     if ( $@ eq "alarm\n" ) {
-        print "Operation timed out after $flags{t} seconds.\n";
+        print "Operation timed out after $flags{timeout} seconds.\n";
         exit 2;
     }
     else {
@@ -57,37 +63,37 @@ if ($@) {
         exit 3;
     }
 }
-else {
 
-    #Divide by 256 or bitshift right by 8 to get the original error code
-    $return_code = $? >> 8;
+
+#Divide by 256 or bitshift right by 8 to get the original error code
+$return_code >>= 8;
 
     #Parse the errors and give Nagios parsible error codes and messages
-    switch ($return_code) {
-        case 0  { print "OK\n";                                     exit 0; }
-        case 64 { print "Command line usage error\n";               exit 1; }
-        case 65 { print "Data format error\n";                      exit 1; }
-        case 66 { print "Cannot open input\n";                      exit 1; }
-        case 67 { print "Addressee unknown\n";                      exit 1; }
-        case 68 { print "Host name unknown\n";                      exit 1; }
-        case 69 { print "Spamd service unavailable\n";              exit 2; }
-        case 70 { print "Internal software error\n";                exit 2; }
-        case 71 { print "System error (e.g., can't fork)\n";        exit 2; }
-        case 72 { print "Critical OS file missing\n";               exit 2; }
-        case 73 { print "Can't create (user) output file\n";        exit 1; }
-        case 74 { print "Input/output error\n";                     exit 1; }
-        case 75 { print "Temp failure; user is invited to retry\n"; exit 1; }
-        case 76 { print "Remote error in protocol\n";               exit 2; }
-        case 77 { print "Permission denied\n";                      exit 1; }
-        case 78 { print "Configuration error\n";                    exit 1; }
-        else    { print "An unknown error has occured in $spamc\n"; exit 3; }
-    }
+switch ($return_code) {
+    case 0  { print "OK\n";                                     exit 0; }
+    case 64 { print "Command line usage error\n";               exit 1; }
+    case 65 { print "Data format error\n";                      exit 1; }
+    case 66 { print "Cannot open input\n";                      exit 1; }
+    case 67 { print "Addressee unknown\n";                      exit 1; }
+    case 68 { print "Host name unknown\n";                      exit 1; }
+    case 69 { print "Spamd service unavailable\n";              exit 2; }
+    case 70 { print "Internal software error\n";                exit 2; }
+    case 71 { print "System error (e.g., can't fork)\n";        exit 2; }
+    case 72 { print "Critical OS file missing\n";               exit 2; }
+    case 73 { print "Can't create (user) output file\n";        exit 1; }
+    case 74 { print "Input/output error\n";                     exit 1; }
+    case 75 { print "Temp failure; user is invited to retry\n"; exit 1; }
+    case 76 { print "Remote error in protocol\n";               exit 2; }
+    case 77 { print "Permission denied\n";                      exit 1; }
+    case 78 { print "Configuration error\n";                    exit 1; }
+    else    { print "An unknown error has occured in $spamc\n"; exit 3; }
 }
 
+
 #Version message information displayed in both --version and --help
-sub main::VERSION_MESSAGE {
+sub main::VersionMessage {
     print <<"EOF";
-This is version $main::VERSION of check_spamd.
+This is version $version of check_spamd.
 
 Copyright (c) 2007 Erinn Looney-Triggs (erinn.looneytriggs\@gmail.com). 
 All rights reserved.
@@ -102,6 +108,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 EOF
 
+exit 1;
 }
 __END__
 
@@ -111,7 +118,7 @@ check_spamd - Checks the status of SpamAssassin's spamd daemon via spamc.
 
 =head1 VERSION
 
-This documentation refers to check_spamd version 1.4
+This documentation refers to check_spamd version 1.5
 
 =head1 USAGE
 
