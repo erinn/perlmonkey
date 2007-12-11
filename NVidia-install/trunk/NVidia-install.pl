e22ff7c29fbacdeb9b5a326330aa2291036be0ee
erinn.looneytriggs <erinn.looneytriggs@53429630-192d-0410-916e-713da7112205>
2007-12-11 05:28:46 +0000

    Initial import.

---
+++ NVidia-install/trunk/NVidia-install.pl
@@ -0,0 +1,302 @@
+#!/usr/local/perl/bin/perl
+
+#####################################################
+#Script to check if the latest version of the nvidia module is installed on
+#the system, if not add it to dkms.
+#Created: 12/7/2007
+#Version: 1.0.0              
+#If you change the version number change it in variables as well as in POD
+#Revised: 
+#Revised by: Erinn Looney-Triggs
+#Author: 
+#####################################################
+
+use Carp;                    #Croak instead of die
+use English qw( -no_match_vars );
+use Getopt::Long;            #Grab command line switches
+use Pod::Usage;
+use POSIX qw( WIFEXITED );   #Fix system call's strange return values
+use strict;                  #Do it right
+use Sys::Hostname;
+use Tie::File;
+use warnings;
+
+my $NVidia_module       = "nvidia-current";
+my $architecture= architecture_check();  #Obtain Arch, either i386 or x86_64
+my $host = hostname;                    #Obtain the hostname
+my $NVidia_directory    = "/usr/local/$NVidia_module" . "_" . "$architecture";
+my $NVidia_installer    = "$NVidia_directory/nvidia-installer";
+my $NVidia_source       = "$NVidia_directory/usr/src/nv/";
+my $NVidia_version;
+my $version_file        = "$NVidia_directory/VERSIONS";
+my $VERSION = "1.0.0";                  #Nvidia-install Version Number
+
+GetOptions(
+            'version|V'     => sub { VersionMessage() },
+            'help|h'        => sub { pod2usage(1) },
+);
+
+rpm_check( "dkms" );
+sanity_checks();
+$NVidia_version = NVidia_version();
+dkms_check();
+
+sub architecture_check{
+    #Capture the architecture output, similair to using uname -a
+    my $arch = (POSIX::uname())[4];
+    
+    #If it is i686 i786 ix86 whatever return arch as i386
+    if ($arch =~ /i.86/){
+        return "i386";
+    }
+    #Else return x86_64 or whatever else it is
+    else{
+        return $arch
+    }
+}
+
+sub dkms_check{
+    my $dkms_output;
+    
+    #Capture the output of dkms status to see if there are nvidia modules 
+    #installed.
+    eval {
+        $dkms_output = `dkms status -m $NVidia_module`; 
+    };
+    if ($?) {
+        croak "Unable to run: dkms status, $OS_ERROR $CHILD_ERROR, aborting.\n";
+    }
+    
+    #If dkms_version exists then find out the version number and compare to 
+    #NVidia's version.
+    if ( $dkms_output =~ /$NVidia_module,\s([\d]+\.[\d]+\.[\d]+)/ ){
+        my $dkms_version = $1;
+        
+        #There is probably a better way to do this, but I don't know it. Get
+        #the version numbers for dkms and NVidia, strip the periods out and 
+        #then do a numerical comparison.
+        my $dkms_version_numeric = $dkms_version;
+        $dkms_version_numeric =~ s/\.//g;
+        
+        my $NVidia_version_numeric = $NVidia_version; 
+        $NVidia_version_numeric =~ s/\.//g;
+        
+        #If the versions do not match remove the dkms version and add the 
+        #NVidia version. This allows upgrades as well as downgrades.
+        if ( $dkms_version_numeric != $NVidia_version_numeric ){
+            
+            #Remove the dkms version and remove the symlink in /usr/src
+            dkms_remove($dkms_version);
+            unlink "/usr/src/$NVidia_module-$dkms_version";
+            
+            #This create the symlink to the new version and adds it to dkms
+            symlink "$NVidia_source", 
+                "/usr/src/$NVidia_module-$NVidia_version";
+            dkms_add($NVidia_version);
+        }        
+    }
+    #If no version exists in dkms create one.
+    else{
+        #Create the symlink to the module source and add the module to dkms
+        symlink "$NVidia_source", "/usr/src/$NVidia_module-$NVidia_version";
+        dkms_add($NVidia_version);
+    }
+    
+    return 0;
+}
+
+sub dkms_add{
+    
+    my $version = shift;    #Get the version number to be added
+    
+    #Add the module to dkms, note this will not build the module that will 
+    #happen on reboot as specified in the dkms.conf file
+    WIFEXITED ( system "dkms add -m $NVidia_module -v $version --quiet" )
+            or croak "Unable to run: 
+                dkms add, $OS_ERROR $CHILD_ERROR, aborting.\n";
+    
+    return 0;   #Return nothing and do it meaningfully ;)
+}
+
+sub dkms_remove{
+    my $dkms_version = shift;   #Get the version number to be removed
+    
+    #This will remove the module and place back any module that was existing
+    #before dkms was installed, it will also remove all the associated NVidia
+    #programs as specified in the dkms.conf file
+    WIFEXITED ( 
+        system "dkms remove -m $NVidia_module -v $dkms_version --all --quiet") 
+            or croak "Unable to run: 
+                dkms remove, $OS_ERROR $CHILD_ERROR, aborting.\n";
+    
+    return 0;   #Return nothing and do it meaningfully ;)
+}
+
+sub NVidia_version{
+    my $version;
+    my @file;
+    
+    #Tie to the versions file readonly and search for the version number
+    tie @file, 'Tie::File', $version_file, mode => "O_RDONLY"
+        or croak "Can't tie to $version_file, $OS_ERROR \n";
+    
+    for my $line (@file){
+        $version = $1 if $line =~ /\s([\d]+\.[\d]+\.[\d]+)\s/;
+    }
+     
+    untie @file;        #Be polite and untie the file
+    
+    return $version;    #Return the version number found
+}
+
+sub rpm_check{
+    my $package = shift;    #Get the package name to be checked for
+    my $result;             #Holds output of rpm command
+    
+    #Make the query to the rpm database to find out if dkms is installed
+    $result = `rpm -q $package`;
+    if ($?){
+        croak "Unable to run: rpm -q $package, aborting.\n";
+    }
+    
+    if ($result){
+        return 0;    #Return nothing and do it meaningfully ;)
+    }
+    
+    #If the package does not exist croak.
+    else {
+        die "The $package rpm is not installed on $host, aborting. \n";
+    }
+}
+
+sub sanity_checks{
+    #The architecture must be either x86_64 or i386 nothing else at this time.
+    unless (($architecture eq "i386") or ($architecture eq "x86_64")){
+        die "Architecture: $architecture, is not supported at this time 
+            on $host, aborting. \n";
+    }
+    #The nvidia directory has to exist, if not croak.
+    if (!-d "$NVidia_directory") {
+        die "$NVidia_directory does not exist on $host, aborting. \n";
+    }
+    #The version file has to exist, if not croak.
+    if (!-e "$version_file"){
+        die "The VERSIONS file does not exist in $NVidia_directory 
+            on $host, aborting. \n";
+    }
+    #The nvidia-installer script has to exist, if not croak.
+    if (!-e "$NVidia_installer"){
+        die "$NVidia_installer does not exist on $host, aborting. \n";
+    }
+    #The nvidia-installer script has to be executable, if not croak.
+    if (!-x "$NVidia_installer"){
+        die "$NVidia_installer is not executableon $host, aborting. \n";
+    }
+    #The NVidia source directory has to exist, if not croak.
+    if (!-d "$NVidia_source"){
+        die "$NVidia_source does not exist on $host, aborting.\n";
+    }
+    #The dkms.conf file has to exist in the NVidia source directory, if not 
+    #croak
+    if (!-e "$NVidia_source/dkms.conf"){
+        die "dkms.conf does not exist in $NVidia_source on 
+            $host, aborting. \n";
+    }
+    
+return 0; #Return nothing and do it meaningfully ;)
+}
+
+sub main::VersionMessage {
+    print <<"EOF";
+This is version $VERSION of nvidia-install.
+
+Copyright (c) 2007 Erinn Looney-Triggs (erinn.looneytriggs\@gmail.com). 
+All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License. 
+See http://www.fsf.org/licensing/licenses/gpl.html
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+
+EOF
+
+    exit 1;
+}
+
+__END__
+=head1 NAME
+
+NVidia-install - Adds the latest distrbuted version of the nvidia module to dkms.
+
+=head1 VERSION
+
+This documentation refers to NVidia-install version 1.0.0
+
+=head1 USAGE
+
+NVidia-install.pl
+
+=head1 REQUIRED ARGUMENTS
+
+None
+
+=head1 OPTIONS
+
+--version       (-V) Display version information
+--help          (-h) Display help information
+
+=head1 DESCRIPTION
+ 
+This is an installation script that will check to see if the nvidia module
+has been installed via dkms. If not it will set up the environment for it and
+add it to dkms so that upon the next reboot the nvidia module will be built
+and installed on the system. If the nvidia module version is different than 
+the existing dkms version the dkms version will be removed and the nvidia 
+version will be added thus bringing the two inline and allowing upgrades.
+
+=head1 DIAGNOSTICS
+
+=head2 
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+dkms must be installed on the system
+ 
+=head1 DEPENDENCIES
+
+    Carp                    Standard Perl 5.8 module
+    English                 Standard Perl 5.8 module
+    Getopt::Long            Standard Perl 5.8 module
+    Pod::Usage              Standard Perl 5.8 module
+    POSIX                   Standard Perl 5.8 module
+    Sys::Hostname           Standard Perl 5.8 module
+    Tie::File               Standard Perl 5.8 module
+    
+=head1 INCOMPATIBILITIES
+
+None known yet.
+
+=head1 BUGS AND LIMITATIONS
+
+Bugs, never heard of 'em ;).
+If you encounter any bugs let me know. (erinn.looneytriggs@gmail.com)
+
+=head1 AUTHOR
+
+Erinn Looney-Triggs (erinn.looneytriggs@gmail.com)
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2007 Erinn Looney-Triggs (erinn.looneytriggs@gmail.com). 
+All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License. 
+See L<http://www.fsf.org/licensing/licenses/gpl.html>.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
